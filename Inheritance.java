a1f5c07aace7a905fd1d0c00eede405a23a0b435
Luckykarthik <118092667+Lucky9652@users.noreply.github.com>
2022-11-14 08:25:52 -0600

    Add files via upload

---
+++ Inheritance.java
@@ -0,0 +1,22 @@
+
+public class Inheritance {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//Syntax==
+		/*class Super{
+			--------------
+			---------------
+
+		}
+		
+class Sub extends Super{
+	---------------
+	----------
+}
+*/
+		
+		
+	}
+
+}
